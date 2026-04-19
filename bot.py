3630bce636308df743ba5e1299b2dbdfe10cc15f
malcomperello <malcomperello@gmail.com>
2026-04-19 22:54:17 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,25 @@
 import tweepy
 import os
-
-auth = tweepy.OAuth1UserHandler(
-    os.environ["API_KEY"],
-    os.environ["API_SECRET"],
-    os.environ["ACCESS_TOKEN"],
-    os.environ["ACCESS_SECRET"]
-)
-
-api = tweepy.API(auth)
+import traceback
 
 try:
+    auth = tweepy.OAuth1UserHandler(
+        os.environ["API_KEY"],
+        os.environ["API_SECRET"],
+        os.environ["ACCESS_TOKEN"],
+        os.environ["ACCESS_SECRET"]
+    )
+
+    api = tweepy.API(auth)
+
+    print("Probando credenciales...")
     api.verify_credentials()
-    api.update_status("SÍ.")
-    print("Tweet enviado")
+    print("Auth OK")
+
+    print("Intentando tweet...")
+    response = api.update_status("SÍ.")
+    print("Tweet enviado:", response)
+
 except Exception as e:
-    print("ERROR:", e)
+    print("ERROR COMPLETO:")
+    traceback.print_exc()
