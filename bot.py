e389aa39d471adf9dee8606ec20f28fd7948dffd
malcomperello <malcomperello@gmail.com>
2026-04-19 22:52:01 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,18 @@
 import tweepy
 import os
 
-api_key = os.environ["API_KEY"]
-api_secret = os.environ["API_SECRET"]
-access_token = os.environ["ACCESS_TOKEN"]
-access_secret = os.environ["ACCESS_SECRET"]
+auth = tweepy.OAuth1UserHandler(
+    os.environ["API_KEY"],
+    os.environ["API_SECRET"],
+    os.environ["ACCESS_TOKEN"],
+    os.environ["ACCESS_SECRET"]
+)
 
-auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_secret)
 api = tweepy.API(auth)
 
-api.update_status("SÍ.")
+try:
+    api.verify_credentials()
+    api.update_status("SÍ.")
+    print("Tweet enviado")
+except Exception as e:
+    print("ERROR:", e)
